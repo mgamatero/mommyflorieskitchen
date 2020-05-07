fdc70455af4c2b31b92b0b25300da58dc5c6fea5
Mike Gamatero <mgamatero@yahoo.com>
2020-05-07 11:09:49 -0700

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageLandingComponent } from './components/page-landing/page-landing.component';
+import { PageNotfoundComponent } from './components/page-notfound/page-notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PageLandingComponent,
+    PageNotfoundComponent
   ],
   imports: [
     BrowserModule,
